1c36867ce9c8534ce351b937051925e816b93462
Stefan Puia <up823744@myport.ac.uk>
2017-11-22 00:34:57 +0000

    done a few tweaks and added some comments

--- README.md
+++ README.md
@@ -6,13 +6,11 @@ Installation
 ```bash
   git clone https://github.com/StefanPuia/File-MD5er.git
   cd File-MD5er
-	
   ```
 
 Usage
 -----
 ```bash
   node index <path>
-
   ```
 The default path is ./files
--- index.js
+++ index.js
@@ -15,22 +15,30 @@ if (!fs.existsSync(donedir)){
     fs.mkdirSync(donedir);
 }
  
-
 readDirFiles.list(path, function (err, filenames) {
 	if (err) return console.dir(err);
+
 	for(let i = 0; i<filenames.length; i++) {
 		let filename = filenames[i];
+		
+		// file is not a directory
 		if(filename.substr(-1) != '/') {
+			// get the filename
 			let parts = filename.split(/\\+/);
 			let file = parts[parts.length-1];
+			
+			// do not change this file if running in current directory
 			if(file != "index.js") {
+				// get the extension
 				let ext = file.split('.')[1];
 
+				// get the md5 of the file
 				md5File(filename, (err, hash) => {
-				  	if (err) 
-				  		throw err
+				  	if (err) throw err
 
+				  	// create new filename
 				  	let newfile = donedir + '/' + hash + '.' + ext;
+				  	// move the file
 					move(filename, newfile, callback);
 				});
 			}			
@@ -67,9 +75,15 @@ function move(oldPath, newPath, callback) {
 }
 
 function callback(err) {
-	if(err.code == 'EPERM') {
-		console.log("File: " + err.path);
-		console.log("Dest: " + err.dest);
-		console.log('File already exists or no write permissions.\n');
+	switch(err.code) {
+		case 'EPERM':
+			console.log("File: " + err.path);
+			console.log("Dest: " + err.dest);
+			console.log('File already exists or no write permissions.\n');
+			break;
+			
+		default:
+			console.log('Unknown error occured.\n');
+			break;
 	}
 }
